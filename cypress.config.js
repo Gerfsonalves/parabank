e5b5da774163368806963445ec03faa0f2bb7328
Gerfson Alves <gerfson17@hotmail.com>
2024-09-02 16:12:18 -0300

    config pasta videos

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,  // Habilita gravação de vídeos
+    videosFolder: 'cypress/videos',  // Diretório onde os vídeos serão salvos
   },
 });
